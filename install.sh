3cc075a8e82e2795831fee203da2ca6cf19eb060
test <test@sd-work2.ece.vt.edu>
2013-02-15 12:48:48 -0500

    added install script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+mydir=$(pwd)
+if [ "" == "${RSTPATH}" ]
+then
+  echo "export RSTPATH=$mydir" >> ~/.bashrc
+  echo ". \$RSTPATH/.profile.bash" >> ~/.bashrc
+fi
+source ~/.bashrc
+echo $mydir
+cd $mydir/codebase/analysis/src.lib/cmpfit-1.2
+make clean
+make
+if [ ! -d "$mydir/include" ]; then
+  mkdir $mydir/include
+fi
+if [ ! -d "$mydir/include/analysis" ]; then
+  mkdir $mydir/include/analysis
+fi
+cp mpfit.h $mydir/include/analysis
+if [ ! -d "$mydir/lib" ]; then
+  mkdir $mydir/lib
+fi
+cp libmpfit.a $mydir/lib
+cd $mydir
+# make.code superdarn rst
